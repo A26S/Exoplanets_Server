c84db64fd13ffa1500693060a2e211fa30f059ce
Ali <saida130.210@gmail.com>
2020-03-28 05:26:08 +0000

    bug fix

--- middleware/passportSetup.js
+++ middleware/passportSetup.js
@@ -3,12 +3,13 @@ dotenv.config()
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
 const { googleConfig } = require('../controllers/o-authController')
+const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = require('../tokens')
 
 passport.use(
     new GoogleStrategy(
     {
-        clientID: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        clientID: GOOGLE_CLIENT_ID,
+        clientSecret: GOOGLE_CLIENT_SECRET,
         callbackURL: '/auth/google/redirect'
     }, 
     googleConfig
