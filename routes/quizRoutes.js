bf048a3e59f1ff6f5cee1230aa194340bd4997c7
Ali <saida130.210@gmail.com>
2020-04-05 19:28:35 +0100

    abstraction

--- routes/quizRoutes.js
+++ routes/quizRoutes.js
@@ -1,59 +1,11 @@
-const express = require('express')
-const QuizResult = require('../models/QuizResult')
-const User = require('../models/User')
-const ErrorHandler = require('../utils/ErrorHandler')
+const router = require('express').Router()
 
-const router = express.Router()
+const { submitQuiz, getResults, removeQuiz } = require('../controllers/quizController')
 
-router.post('/submit', async (req, res, next) => {
-    const { grade, user } = req.body
-    const result = await QuizResult.create({
-        grade,
-        user: user._id
-    })
-    if (result) {
-        const dbUser = await User.findById(result.user._id)
-        dbUser.quizResults.push(result)
-        try {
-            await dbUser.save()
-        } catch (err) {
-            next(err)
-        }
-        return res.json({
-            message: "success",
-            result
-        })    
-    }
-    const err = new ErrorHandler('could not create survey', 500)
-    next(err)
-})
+router.post('/submit', submitQuiz)
 
-router.get('/:userId', async (req, res, next) => {
-    const { userId } = req.params
-    const user = await User.findById(userId).populate('quizResults')
-    console.log(user)
-    return res.json(
-        user.quizResults
-    )
-})
+router.get('/:userId', getResults)
 
-router.get('/remove/:quizId', async (req, res, next) => {
-    const { quizId } = req.params
-    try {
-        const quiz = await QuizResult.findById(quizId).populate('user')
-        // console.log(quiz)
-        // quiz.user.quizResults.filter(quizResult => quizResult !== quizId)
-        try {
-            await quiz.remove()
-        } catch (err) {
-            next(err)
-        }
-    } catch (err) {
-        next(err)
-    }
-    return res.json({
-        lol: 'nice'
-    })
-})
+router.get('/remove/:quizId', removeQuiz)
 
 module.exports = router
